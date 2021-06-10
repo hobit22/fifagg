36b318c82edee6f44090eb8d1c67e24db41090a4
hobit22 <hobit22@naver.com>
2021-06-10 10:05:03 +0900

    1

--- web.js
+++ web.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 app.set('port', 8001);
 app.set('view engine', 'html');
-nunjucks.configure('/home/hosting_users/hobit2404/apps/hobit2404_fifagg/views', {
+nunjucks.configure('/home/hosting_users/hobit2404/apps/hobit2404_ffgg/views', {
 	express : app,
 	watch : true,
 });
